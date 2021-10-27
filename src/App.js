f37015fe967215d43b736a13132d3373523bc8bc
Kevin Salter <kevinsalter.developer@gmail.com>
2021-10-26 20:54:50 -0700

    add a basic sign up form with validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,153 @@
-import logo from './logo.svg';
 import './App.css';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { validationSchema } from './utils';
 
-function App() {
+export default function App() {
+  const formOptions = { resolver: yupResolver(validationSchema) };
+  const { register, handleSubmit, reset, formState } = useForm(formOptions);
+  const { errors } = formState;
+
+  function onSubmit(data) {
+    console.log(JSON.stringify(data, null, 4));
+    return false;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page-wrap">
+      <div className="card m-3">
+        <h5 className="card-header">Sign Me Up!</h5>
+        <div className="card-body">
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-row">
+              <div className="form-group col">
+                <label>Title</label>
+                <select
+                  name="title"
+                  {...register('title')}
+                  className={`form-control ${errors.title ? 'is-invalid' : ''}`}
+                >
+                  <option value=""></option>
+                  <option value="Mr">Mr</option>
+                  <option value="Mrs">Mrs</option>
+                  <option value="Miss">Miss</option>
+                  <option value="Ms">Ms</option>
+                </select>
+                <div className="invalid-feedback">{errors.title?.message}</div>
+              </div>
+              <div className="form-group col-5">
+                <label>First Name</label>
+                <input
+                  name="firstName"
+                  type="text"
+                  {...register('firstName')}
+                  className={`form-control ${
+                    errors.firstName ? 'is-invalid' : ''
+                  }`}
+                />
+                <div className="invalid-feedback">
+                  {errors.firstName?.message}
+                </div>
+              </div>
+              <div className="form-group col-5">
+                <label>Last Name</label>
+                <input
+                  name="lastName"
+                  type="text"
+                  {...register('lastName')}
+                  className={`form-control ${
+                    errors.lastName ? 'is-invalid' : ''
+                  }`}
+                />
+                <div className="invalid-feedback">
+                  {errors.lastName?.message}
+                </div>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col">
+                <label>Date of Birth</label>
+                <input
+                  name="dob"
+                  type="date"
+                  {...register('dob')}
+                  className={`form-control ${errors.dob ? 'is-invalid' : ''}`}
+                />
+                <div className="invalid-feedback">{errors.dob?.message}</div>
+              </div>
+              <div className="form-group col">
+                <label>Email</label>
+                <input
+                  name="email"
+                  type="text"
+                  {...register('email')}
+                  className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                />
+                <div className="invalid-feedback">{errors.email?.message}</div>
+              </div>
+            </div>
+            <div className="form-row">
+              <div className="form-group col">
+                <label>Password</label>
+                <input
+                  name="password"
+                  type="password"
+                  {...register('password')}
+                  className={`form-control ${
+                    errors.password ? 'is-invalid' : ''
+                  }`}
+                />
+                <div className="invalid-feedback">
+                  {errors.password?.message}
+                </div>
+              </div>
+              <div className="form-group col">
+                <label>Confirm Password</label>
+                <input
+                  name="confirmPassword"
+                  type="password"
+                  {...register('confirmPassword')}
+                  className={`form-control ${
+                    errors.confirmPassword ? 'is-invalid' : ''
+                  }`}
+                />
+                <div className="invalid-feedback">
+                  {errors.confirmPassword?.message}
+                </div>
+              </div>
+            </div>
+            <div className="form-group form-check">
+              <input
+                name="acceptTerms"
+                type="checkbox"
+                {...register('acceptTerms')}
+                id="acceptTerms"
+                className={`form-check-input ${
+                  errors.acceptTerms ? 'is-invalid' : ''
+                }`}
+              />
+              <label htmlFor="acceptTerms" className="form-check-label">
+                Accept Terms & Conditions
+              </label>
+              <div className="invalid-feedback">
+                {errors.acceptTerms?.message}
+              </div>
+            </div>
+            <div className="form-group">
+              <button type="submit" className="btn btn-primary mr-1">
+                Register
+              </button>
+              <button
+                type="button"
+                onClick={() => reset()}
+                className="btn btn-secondary"
+              >
+                Reset
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
